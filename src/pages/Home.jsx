83af7ab38cda5a0017809e4818beae6e4335eec0
Joshua Park <joshuapark1309@gmail.com>
2025-06-14 18:16:52 -0400

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">
+      {/* Theme Togg */}
+
+      {/* Background Effect */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
