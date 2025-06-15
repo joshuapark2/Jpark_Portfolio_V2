5a46af30bdb527e7e8106b445d86ebd7102024c9
Joshua Park <joshuapark1309@gmail.com>
2025-06-14 21:44:18 -0400

    Skills Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,7 +2,7 @@ import { Briefcase, Code, User } from "lucide-react";
 
 export const AboutSection = () => {
   return (
-    <section id="#about" className="py-24 px-4 relative">
+    <section id="about" className="py-24 px-4 relative">
       {" "}
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
@@ -16,9 +16,9 @@ export const AboutSection = () => {
               Passionate Web Developer & Tech Creator
             </h3>
             <p className="text-muted-foreground">
-              With over 5 years of experience i nweb development, I specialize
+              With over 5 years of experience in web development, I specialize
               in creating responsive, accessible, and performant web
-              applications using modenr technologies.
+              applications using modern technologies.
             </p>
             <p className="text-muted-foreground">
               I am passionate about creating elegant solutions to complex
